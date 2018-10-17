f4167fc5160bfa0b50114a4bd18d5f39c4e05978
Joseph Hnatek <jhnatek07@gmail.com>
2018-10-16 20:11:57 -0500

    Added FormsModule and ReactiveFormsModule for JSON input for peer to peer WebRTC capabilities.

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {InstrumentComponent} from './instrument/instrument.component';
@@ -25,7 +25,9 @@ import { VideoComponent } from './video/video.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
