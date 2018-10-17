94624b9fe5ddcf87ddabdee67f571f320ab5c761
Joseph Hnatek <jhnatek07@gmail.com>
2018-10-16 20:13:30 -0500

    Added peer to peer WebRTC functionality.

--- ui/src/app/video/video.component.ts
+++ ui/src/app/video/video.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+declare var SimplePeer: any;
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
@@ -9,31 +9,63 @@ export class VideoComponent implements OnInit {
 
   @ViewChild('hardwareVideo') hardwareVideo: any;
 
-  navigator = <any> navigator;
-  localStream;
+  targetpeer: any;
+  peer: any;
+  navigator = <any>navigator;
 
   ngOnInit() {
 
-    //size of webcam stream
     const constraints =
       {
         audio: false,
-        video: {width: 900, height: 566},
-        frameRate: {ideal: 30, max: 60}
+        video: {width: 900, height: 566}
       };
 
     const video = this.hardwareVideo.nativeElement;
-    this.navigator = <any>navigator;
+    let p2p: any;
 
-    this.navigator.getUserMedia = (this.navigator.getUserMedia || this.navigator.webkitGetUserMedia
-      || this.navigator.mozGetUserMedia || this.navigator.msGetUserMedia);
+    this.navigator.getUserMedia = (this.navigator.getUserMedia || this.navigator.webkitGetUserMedia ||
+                                    this.navigator.mozGetUserMedia || this.navigator.msGetUserMedia);
 
-    this.navigator.mediaDevices.getUserMedia(constraints)
-      .then((stream) => {
-        this.localStream = stream;
-        video.src = window.URL.createObjectURL(stream);
-        video.play();
-      });
+    this.navigator.getUserMedia(constraints, function(stream) {
 
+    p2p = new SimplePeer ({
+      initiator: location.hash === '#init',
+      trickle: false,
+      stream:stream
+    })
+
+    p2p.on('signal', function(data) {
+      console.log(JSON.stringify(data));
+
+      this.targetpeer = data;
+    })
+
+    p2p.on('data', function(data) {
+      console.log('Recieved message:' + data);
+    })
+
+    p2p.on('stream', function(stream) {
+      video.src = URL.createObjectURL(stream);
+      video.play();
+    })
+
+    }, function(err){
+    console.log('Failed to get stream', err);
+    });
+
+    setTimeout(() => {
+      this.peer = p2p;
+      console.log(this.peer);
+    }, 5000);
   }
+
+  connect() {
+    this.peer.signal(JSON.parse(this.targetpeer));
+  }
+
+  message() {
+    this.peer.send('p2p connected');
+  }
+
 }
