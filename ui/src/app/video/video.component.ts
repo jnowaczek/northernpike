00247c7e96a0a3a9e50911a665452e147bbe7105
Tom Zurales <tomzurales@gmail.com>
2018-10-22 22:21:32 -0500

    Added ability to change mjpg video source IP from UI

--- ui/src/app/video/video.component.ts
+++ ui/src/app/video/video.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import Hls from 'hls.js';
 
 @Component({
   selector: 'app-video',
@@ -8,26 +7,17 @@ import Hls from 'hls.js';
 })
 export class VideoComponent implements OnInit {
 
-  video: HTMLVideoElement;
+  streamIp = '127.0.0.1';
 
   constructor() {
   }
 
   ngOnInit() {
-    this.video = <HTMLVideoElement> document.getElementById('video');
+  }
 
-    if (Hls.isSupported()) {
-      const hls = new Hls();
-      hls.attachMedia(this.video);
-      hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-        console.log('video and hls.js are now bound together !');
-        hls.loadSource('https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8');
-        hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-          console.log('manifest loaded, found ' + data.levels.length + ' quality level');
-        });
-      });
-      this.video.play();
-    }
+  setStreamIp(){
+    this.streamIp = streamIpInput.value;
+    console.log(this.streamIp);
   }
 
 }
