0d81990441fd7fd0021ba10867f10f4b9cff83ac
Tom Zurales <tomzurales@gmail.com>
2018-10-22 22:49:20 -0500

    Fix for undeclared variable build issue

--- ui/src/app/video/video.component.ts
+++ ui/src/app/video/video.component.ts
@@ -8,15 +8,17 @@ import {Component, OnInit} from '@angular/core';
 export class VideoComponent implements OnInit {
 
   streamIp = '127.0.0.1';
+  streamIpInput: HTMLInputElement;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.streamIpInput = <HTMLInputElement> document.getElementById('streamIpInput');
   }
 
   setStreamIp(){
-    this.streamIp = streamIpInput.value;
+    this.streamIp = this.streamIpInput.value;
     console.log(this.streamIp);
   }
 
