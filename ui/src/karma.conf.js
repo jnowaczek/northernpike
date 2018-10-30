c45b54fefa6b75a5b1146f82bb64a359a2950f64
jnowaczek <jnowaczek@users.noreply.github.com>
2018-10-30 15:16:23 -0500

    Configure Travis CI Angular tests

--- ui/src/karma.conf.js
+++ ui/src/karma.conf.js
@@ -24,8 +24,8 @@ module.exports = function (config) {
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
+    logLevel: config.LOG_WARN,
+    autoWatch: false,
     browsers: ['Firefox'],
     singleRun: true
   });
