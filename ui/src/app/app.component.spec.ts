205a117057d794ba26c3e8355133f05b9686b74e
jnowaczek <jnowaczek@users.noreply.github.com>
2018-10-30 14:35:43 -0500

    Verify Angular (Jasmine) tests work on dev machine, separate camera views into second Angular module

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -1,11 +1,31 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {SwitchesComponent} from './switches/switches.component';
+import {LinearGaugeComponent} from './linear-gauge/linear-gauge.component';
+import {RadialGaugeComponent} from './radial-gauge/radial-gauge.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {WebsocketService} from './websocket.service';
+import {RovStateService} from './rovstate.service';
+import {ViewportModule} from './viewport/viewport.module';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        SwitchesComponent,
+        LinearGaugeComponent,
+        RadialGaugeComponent,
       ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        NgbModule,
+        ViewportModule
+      ],
+      providers: [WebsocketService, RovStateService]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +33,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'controlpanel'`, async(() => {
+  it(`should have as title 'Northern Pike Control Panel'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ui');
+    expect(app.title).toEqual('Northern Pike Control Panel');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ui!');
+    expect(compiled.querySelector('h1').textContent).toContain('Northern Pike Control Panel');
   }));
 });
