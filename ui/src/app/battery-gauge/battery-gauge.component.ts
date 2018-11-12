448094ba6305b6121fb4e824ff00fa538b08763d
Joseph Hnatek <24803352+fahnatek@users.noreply.github.com>
2018-11-11 21:08:41 -0600

    Added a light-slider, battery gauge, pressure gauge, temperature gauge, and a different component for switches.

---
+++ ui/src/app/battery-gauge/battery-gauge.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-battery-gauge',
+  templateUrl: './battery-gauge.component.html',
+  styleUrls: ['./battery-gauge.component.css']
+})
+export class BatteryGaugeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  gaugeType = "semi";
+    gaugeValue = 70;
+    gaugeLabel = "Battery";
+    gaugeAppendText = "%";
+    gaugeMin = "0";
+    gaugeMax = "100";
+    gaugeSize = "75";
+
+    thresholdConfig = {
+        '75': {color: 'green'},
+        '50': {color: 'orange'},
+        '25': {color: 'red'}
+    };
+
+}
