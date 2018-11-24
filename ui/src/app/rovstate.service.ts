e886aa5c97c02c79548f4ea7ff3ee3ba5dd2d802
jnowaczek <jnowaczek@users.noreply.github.com>
2018-11-24 13:54:54 -0500

    Pylint should ignore generated protobuf code.

--- ui/src/app/rovstate.service.ts
+++ ui/src/app/rovstate.service.ts
@@ -1,15 +1,14 @@
-import {Injectable} from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
 import {BehaviorSubject, Subject} from 'rxjs';
 import {WebsocketService} from './websocket.service';
-import {RovState} from './RovState';
-import {Telemetry} from './telemetry';
+import {Message, load} from 'protobufjs';
 
 const WS_URL = 'ws://' + location.host + '/ws';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RovStateService {
+export class RovStateService implements OnInit {
 
   public state = new BehaviorSubject(new RovState());
   private socket: Subject<MessageEvent>;
@@ -17,11 +16,45 @@ export class RovStateService {
   constructor(websocket: WebsocketService) {
     this.socket = websocket.connect(WS_URL);
     this.socket.subscribe((msg: MessageEvent) => {
-      this.state.next(JSON.parse(msg.data));
+      this.parseMessage(msg);
     });
   }
 
   public pushState(newState: RovState): void {
     this.socket.next(new MessageEvent('telemetry', {data: newState}));
   }
+
+  private parseMessage(msg: MessageEvent): void {
+    const decoded = Message.decode(msg.data);
+    decoded.$type;
+  }
+
+  ngOnInit() {
+    load('shared/protos/telemetry.proto', (error) => {
+      if (error) {
+        throw error;
+      }
+    });
+  }
+}
+
+export class Switch {
+  label: string;
+  value: boolean;
+}
+
+export const SWITCHES: Switch[] = [
+  {label: 'LED', value: false},
+  {label: 'S 2', value: false},
+  {label: 'S 3', value: false},
+  {label: 'S 4', value: false},
+  {label: 'S 5', value: false},
+];
+
+export class RovState {
+  static switchState: Switch[];
+  static ledIntensity: number;
+  static batteryVoltage: number;
+  static water: boolean;
+  static temperature: number;
 }
